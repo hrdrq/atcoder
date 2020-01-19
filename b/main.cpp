451e25939c5d4d0d6daee68d9097dc0df25b0b13
admin <admin@adminnoMacBook-Pro.local>
2020-01-19 18:21:52 +0900

    use dir and modify makefile

--- b/main.cpp
+++ b/main.cpp
@@ -7,13 +7,22 @@
 #endif
 
 using namespace std;
+typedef long long ll;
 
 int main() {
 #ifdef HIRO
-  ifstream cin("input.txt");
+  string file_path(QUIZ);
+  ifstream cin(file_path + "/input.txt");
   std::cin.rdbuf(cin.rdbuf());
 #endif
+  // int n;
+  // int m;
+  // cin >> n;
+  // cin >> m;
+  
 
+
+  // cout << ans << endl;
   return 0;
 }
 
