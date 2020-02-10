23af273f282f106adaf1bb314cff76ade32705bb
Son munehiro <son.munehiro+1@gmail.com>
2020-02-10 19:13:53 +0900

    add macros, funcs, and modify makefile

--- f/main.cpp
+++ f/main.cpp
@@ -1,13 +1,53 @@
+#include <bits/stdc++.h>
 #ifdef HIRO
-#include "../stdc++.h"
 #define d(f_, ...) printf((f_), ##__VA_ARGS__)
 #else
-#include <bits/stdc++.h>
 #define d(f_, ...)
 #endif
 
 using namespace std;
 typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+#define mp make_pair
+#define pi 3.14159265358979323846
+#define MAX max_element
+#define MIN min_element
+#define all(v) v.begin(), v.end()
+#define gt(T) greater<T>()
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+#define uni(v) do{ \
+  sort(v.begin(), v.end()); \
+  decltype(v)::iterator result = unique(v.begin(), v.end()); \
+  v.erase(result, v.end()); \
+}while(0)
+#define fi(init, n) for(int i = init; i < n; ++i)
+#define fj(init, n) for(int j = init; j < n; ++j)
+#define fk(init, n) for(int k = init; k < n; ++k)
+const ll MOD = 1e9 + 7;
+const ll INF = (ll)1e18;
+
+template<typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+template<typename T>
+T lcm(T a, T b) {
+	return a * b / gcd(a, b);
+}
+template<typename T>
+int finder(vector<T> vec, T number) {
+	auto itr = find(vec.begin(), vec.end(), number);
+	size_t index = distance(vec.begin(), itr);
+	if (index != vec.size()) return 1;
+	else return 0;
+}
+ll frac(ll n){
+	if (n == 0) return 1;
+	return (n * frac(n - 1)) % MOD;
+}
 
 int main() {
 #ifdef HIRO
@@ -15,13 +55,14 @@ int main() {
   ifstream cin(file_path + "/input.txt");
   std::cin.rdbuf(cin.rdbuf());
 #endif
-  // int n;
-  // int m;
+  ll ans = 0;
+  // ll n;
   // cin >> n;
-  // cin >> m;
+  // ll n, m;
+  // cin >> n >> m;
+  // ll n, m, k;
+  // cin >> n >> m >> k;
   
-
-
   // cout << ans << endl;
   return 0;
 }
